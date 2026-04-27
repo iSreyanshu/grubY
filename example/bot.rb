5839b8626f50eeba8d7910d043faffed7660f76a
Sreyanshu <sreyanshu@mail.ru>
2026-04-27 10:38:12 +0530

    updated!

--- example/bot.rb
+++ example/bot.rb
@@ -8,28 +8,28 @@ client.use_plugin("../plugins/logger.rb")
 
 HELP = <<~TXT
   Commands:
-  /start - boot check
-  /help - show this help
-  /echo <text> - echo text
-  /md <text> - markdown v2 safe send
-  /html <text> - html safe send
-  /poll - send sample poll
-  /media - send sample media methods
-  /whoami - show user + chat info
-  /warn <user_id> [reason] - warn user
-  /mute <user_id> - mute user
-  /unmute <user_id> - unmute user
-  /ban <user_id> - ban user
-  /unban <user_id> - unban user
-  /pin - pin current message
-  /unpin - unpin latest pinned message
-  /lock - lock group
-  /unlock - unlock group
-  /reaction - set reaction on replied message
+  /start - Boot check
+  /help - Show this help message
+  /echo <text> - Echo text
+  /md <text> - Markdown v2 safe send
+  /html <text> - HTML safe send
+  /poll - Send sample poll
+  /media - Send sample media methods
+  /whoami - Show user and chat info
+  /warn <user_ID> [reason] - Warn user
+  /mute <user_ID> - Mute user
+  /unmute <user_ID> - Unmute user
+  /ban <user_ID> - Ban user
+  /unban <user_ID> - Unban user
+  /pin - Pin message
+  /unpin - Unpin latest pinned message
+  /lock - Lock group
+  /unlock - Unlock group
+  /reaction - Set reaction on replied message
 TXT
 
 client.on(:message, GrubY::Filters.command("start")) do |ctx|
-  ctx.reply("GrubY client started. Use /help")
+  ctx.reply("GrubY Client started! Use /help for help message.")
 end
 
 client.on(:message, GrubY::Filters.command("help")) do |ctx|
@@ -38,7 +38,7 @@ end
 
 client.on(:message, GrubY::Filters.command("echo")) do |ctx|
   text = ctx.command_args.join(" ")
-  ctx.reply(text.empty? ? "Usage: /echo hello" : text)
+  ctx.reply(text.empty? ? "Usage: /echo good morning" : text)
 end
 
 client.on(:message, GrubY::Filters.command("md")) do |ctx|
@@ -56,11 +56,11 @@ end
 client.on(:message, GrubY::Filters.command("poll")) do |ctx|
   client.send_poll(
     ctx.chat_id,
-    "Pick stack",
+    "Pick Stack",
     [
       { text: "Ruby" },
       { text: "Python" },
-      { text: "Go" }
+      { text: "JavaScript" }
     ],
     is_anonymous: false,
     allows_multiple_answers: true
@@ -83,13 +83,13 @@ client.on(:message, GrubY::Filters.command("warn")) do |ctx|
   user_id = ctx.command_args[0]&.to_i
   reason = ctx.command_args[1..]&.join(" ")
   if user_id.nil? || user_id.zero?
-    ctx.reply("Usage: /warn <user_id> [reason]")
+    ctx.reply("Usage: /warn <user_ID> [reason]")
     next
   end
 
   result = gm.enforce_warns(ctx.chat_id, user_id, reason: reason)
   if result[:action] == :kick
-    ctx.reply("User #{user_id} kicked. warn limit reached.")
+    ctx.reply("User #{user_id} has been kicked. Reason: Warning limit exceeded.")
   else
     ctx.reply("Warned #{user_id}. #{result[:warns]}/#{result[:limit]}")
   end
@@ -97,7 +97,7 @@ end
 
 client.on(:message, GrubY::Filters.command("mute")) do |ctx|
   user_id = ctx.command_args[0]&.to_i
-  next ctx.reply("Usage: /mute <user_id>") if user_id.nil? || user_id.zero?
+  next ctx.reply("Usage: /mute <user_ID>") if user_id.nil? || user_id.zero?
 
   gm.mute(ctx.chat_id, user_id)
   ctx.reply("Muted #{user_id}")
@@ -105,7 +105,7 @@ end
 
 client.on(:message, GrubY::Filters.command("unmute")) do |ctx|
   user_id = ctx.command_args[0]&.to_i
-  next ctx.reply("Usage: /unmute <user_id>") if user_id.nil? || user_id.zero?
+  next ctx.reply("Usage: /unmute <user_ID>") if user_id.nil? || user_id.zero?
 
   gm.unmute(ctx.chat_id, user_id)
   ctx.reply("Unmuted #{user_id}")
@@ -113,7 +113,7 @@ end
 
 client.on(:message, GrubY::Filters.command("ban")) do |ctx|
   user_id = ctx.command_args[0]&.to_i
-  next ctx.reply("Usage: /ban <user_id>") if user_id.nil? || user_id.zero?
+  next ctx.reply("Usage: /ban <user_ID>") if user_id.nil? || user_id.zero?
 
   gm.ban(ctx.chat_id, user_id)
   ctx.reply("Banned #{user_id}")
@@ -121,7 +121,7 @@ end
 
 client.on(:message, GrubY::Filters.command("unban")) do |ctx|
   user_id = ctx.command_args[0]&.to_i
-  next ctx.reply("Usage: /unban <user_id>") if user_id.nil? || user_id.zero?
+  next ctx.reply("Usage: /unban <user_ID>") if user_id.nil? || user_id.zero?
 
   gm.unban(ctx.chat_id, user_id, only_if_banned: true)
   ctx.reply("Unbanned #{user_id}")
@@ -129,33 +129,32 @@ end
 
 client.on(:message, GrubY::Filters.command("pin")) do |ctx|
   gm.pin(ctx.chat_id, ctx.message.message_id)
-  ctx.reply("Pinned this message")
+  ctx.reply("Pinned this message.")
 end
 
 client.on(:message, GrubY::Filters.command("unpin")) do |ctx|
   gm.unpin(ctx.chat_id)
-  ctx.reply("Unpinned latest message")
+  ctx.reply("Unpinned latest message.")
 end
 
 client.on(:message, GrubY::Filters.command("lock")) do |ctx|
   gm.lock(ctx.chat_id)
-  ctx.reply("Group locked")
+  ctx.reply("Group locked!")
 end
 
 client.on(:message, GrubY::Filters.command("unlock")) do |ctx|
   gm.unlock(ctx.chat_id)
-  ctx.reply("Group unlocked")
+  ctx.reply("Group unlocked!")
 end
 
 client.on(:message, GrubY::Filters.command("reaction")) do |ctx|
   message_id = ctx.message.message_id
   client.send_reaction(ctx.chat_id, message_id, reaction: [{ type: "emoji", emoji: "\u{1F525}" }])
-  ctx.reply("Reaction sent")
+  ctx.reply("Reaction sent.")
 end
 
 client.on(:message, GrubY::Filters.regex(/hello/i)) do |ctx|
-  ctx.reply("hello world from GrubY")
+  ctx.reply("Hello world from GrubY!")
 end
 
 client.run
-
