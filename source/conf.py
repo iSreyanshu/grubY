dae7485f15cc3582df7983f14fee88628032ae38
Sreyanshu <sreyanshu@mail.ru>
2026-04-27 15:34:20 +0530

    updated!

--- source/conf.py
+++ source/conf.py
@@ -1,5 +1,18 @@
 import sphinx_rtd_theme
 
-extensions = ['myst_parser', 'sphinx_rtd_theme']
+project = 'grubY'
+copyright = '2026, Sreyanshu'
+author = 'Sreyanshu'
+
+extensions = [
+    'myst_parser',
+    'sphinx_rtd_theme',
+]
+
 html_theme = 'sphinx_rtd_theme'
-source_suffix = {'.rst': 'restructuredtext', '.md': 'markdown'}
+html_theme_options = {
+    'display_version': False,
+    'prev_next_buttons_location': 'bottom',
+}
+html_show_sphinx = False
+html_show_sourcelink = False
