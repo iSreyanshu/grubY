e1587c6f624bed63cb8c66ecbbe9ff7a222f29c2
Sreyanshu <sreyanshu@mail.ru>
2026-04-28 11:30:10 +0530

    updated!

--- lib/grubY/raw.rb
+++ lib/grubY/raw.rb
@@ -1,8 +1,25 @@
-﻿module GrubY
+module GrubY
   module Raw
-    def self.call(api, method, params={})
+    module_function
+
+    def call(api, method, params = {})
       api.raw(method, params)
+    end
+
+    def td_call(td_client, query, timeout: 30.0)
+      if td_client.respond_to?(:raw)
+        td_client.raw(query, timeout: timeout)
+      else
+        td_client.invoke(query, timeout: timeout)
+      end
+    end
+
+    def td_call!(td_client, query, timeout: 30.0)
+      response = td_call(td_client, query, timeout: timeout)
+      if response.is_a?(Hash) && response["@type"] == "error"
+        raise StandardError, "TDLib raw error: #{response['message']}"
+      end
+      response
     end
   end
 end
-
