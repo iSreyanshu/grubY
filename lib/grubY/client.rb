42160dd2ea61c6671776468d3d04a7eee2c45bd7
Sreyanshu <sreyanshu@mail.ru>
2026-04-28 11:42:16 +0530

    updated!

--- lib/grubY/client.rb
+++ lib/grubY/client.rb
@@ -1,5 +1,10 @@
 require_relative "bot"
 require_relative "group_manager"
+require_relative "handlers"
+require_relative "filters"
+require_relative "types/message"
+require_relative "types/chat"
+require_relative "types/user"
 
 module GrubY
   class Client
@@ -18,15 +23,19 @@ module GrubY
     def start
       return if @running
 
+      @bot.emit(:connect)
       @running = true
       start_polling
+      @bot.emit(:start)
       self
     end
 
     def stop
+      @bot.emit(:stop) if @running
       @running = false
       @polling_thread&.kill
       @polling_thread = nil
+      @bot.emit(:disconnect)
       self
     end
 
@@ -53,8 +62,12 @@ module GrubY
       stop
     end
 
-    def add_handler(event, filter=nil, &block)
-      @bot.on(event, filter, &block)
+    def add_handler(handler_or_event, filter=nil, group: 0, &block)
+      if handler_or_event.is_a?(GrubY::Handlers::BaseHandler)
+        register_handler_object(handler_or_event)
+      else
+        @bot.on(handler_or_event, filter, group: group, &block)
+      end
     end
 
     def remove_handler(event, handler_id = nil, &block)
@@ -63,6 +76,51 @@ module GrubY
 
     def on(event, filter=nil, &block)
       add_handler(event, filter, &block)
+    end
+
+    {
+      on_message: GrubY::Handlers::MessageHandler,
+      on_edited_message: GrubY::Handlers::EditedMessageHandler,
+      on_business_message: GrubY::Handlers::BusinessMessageHandler,
+      on_edited_business_message: GrubY::Handlers::EditedBusinessMessageHandler,
+      on_deleted_messages: GrubY::Handlers::DeletedMessagesHandler,
+      on_deleted_business_messages: GrubY::Handlers::DeletedBusinessMessagesHandler,
+      on_message_reaction_count: GrubY::Handlers::MessageReactionCountHandler,
+      on_message_reaction: GrubY::Handlers::MessageReactionHandler,
+      on_business_connection: GrubY::Handlers::BusinessConnectionHandler,
+      on_story: GrubY::Handlers::StoryHandler,
+      on_callback_query: GrubY::Handlers::CallbackQueryHandler,
+      on_chat_boost: GrubY::Handlers::ChatBoostHandler,
+      on_chat_join_request: GrubY::Handlers::ChatJoinRequestHandler,
+      on_chat_member_updated: GrubY::Handlers::ChatMemberUpdatedHandler,
+      on_chosen_inline_result: GrubY::Handlers::ChosenInlineResultHandler,
+      on_inline_query: GrubY::Handlers::InlineQueryHandler,
+      on_poll: GrubY::Handlers::PollHandler,
+      on_pre_checkout_query: GrubY::Handlers::PreCheckoutQueryHandler,
+      on_purchased_paid_media: GrubY::Handlers::PurchasedPaidMediaHandler,
+      on_shipping_query: GrubY::Handlers::ShippingQueryHandler,
+      on_user_status: GrubY::Handlers::UserStatusHandler,
+      on_start: GrubY::Handlers::StartHandler,
+      on_stop: GrubY::Handlers::StopHandler,
+      on_connect: GrubY::Handlers::ConnectHandler,
+      on_disconnect: GrubY::Handlers::DisconnectHandler,
+      on_managed_bot: GrubY::Handlers::ManagedBotUpdatedHandler,
+      on_raw_update: GrubY::Handlers::RawUpdateHandler
+    }.each do |decorator_name, klass|
+      define_method(decorator_name) do |filters = nil, group: 0, &block|
+        add_handler(klass.new(block, filters: filters, group: group))
+      end
+    end
+
+    def on_error(exceptions = nil, filters = nil, group: 0, &block)
+      add_handler(
+        GrubY::Handlers::ErrorHandler.new(
+          block,
+          filters: filters,
+          group: group,
+          exceptions: exceptions
+        )
+      )
     end
 
     def use(&block)
@@ -111,6 +169,18 @@ module GrubY
       @api.get_file(file_id)
     end
 
+    def build_message(data)
+      GrubY::Message.new(data, api: @api, client: self)
+    end
+
+    def build_chat(data)
+      GrubY::Chat.new(data, api: @api, client: self)
+    end
+
+    def build_user(data)
+      GrubY::User.new(data, api: @api, client: self)
+    end
+
     def idle
       @idle = true
       trap_signals
@@ -154,6 +224,49 @@ module GrubY
 
     private
 
+    def register_handler_object(handler)
+      wrapped = proc do |ctx|
+        payload = handler.extract(ctx)
+        if handler.filters
+          next unless handler.filters.call(handler, self, payload)
+        end
+
+        if handler.is_a?(GrubY::Handlers::ErrorHandler)
+          error = payload
+          allowed = Array(handler.exceptions).compact
+          next if !allowed.empty? && !allowed.any? { |klass| error.is_a?(klass) }
+          invoke_handler_callback(handler.callback, error, ctx)
+        else
+          invoke_handler_callback(handler.callback, payload, ctx)
+        end
+      end
+
+      @bot.on(handler.event, nil, group: handler.group, &wrapped)
+    end
+
+    def invoke_handler_callback(callback, payload, ctx)
+      return unless callback
+      if payload.nil? && ctx.update.is_a?(Hash) && ctx.update.empty?
+        case callback.arity
+        when 0 then callback.call
+        when 1 then callback.call(self)
+        else callback.call(self, nil, ctx)
+        end
+        return
+      end
+
+      case callback.arity
+      when 0
+        callback.call
+      when 1
+        callback.call(payload)
+      when 2
+        callback.call(self, payload)
+      else
+        callback.call(self, payload, ctx)
+      end
+    end
+
     def start_polling
       offset = nil
       @polling_thread = Thread.new do
@@ -165,6 +278,7 @@ module GrubY
               process_update(update)
             end
           rescue => e
+            @bot.emit(:error, { "error" => e })
             warn "[CLIENT] polling error: #{e.class}: #{e.message}"
             sleep 1
           end
