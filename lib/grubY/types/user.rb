ed2f1526be1ac24b4e53ffe31781bc5226b1b432
Sreyanshu <sreyanshu@mail.ru>
2026-04-28 11:58:28 +0530

    updated!

--- lib/grubY/types/user.rb
+++ lib/grubY/types/user.rb
@@ -7,5 +7,25 @@ module GrubY
       :can_read_all_group_messages, :supports_inline_queries,
       :can_connect_to_business, :has_main_web_app, :has_topics_enabled,
       :allows_users_to_create_topics, :can_manage_bots
+
+    def archive
+      call_raw_api("archiveChats", { chat_ids: [id] })
+    end
+
+    def unarchive
+      call_raw_api("unarchiveChats", { chat_ids: [id] })
+    end
+
+    def block
+      call_raw_api("blockUser", { user_id: id })
+    end
+
+    def unblock
+      call_raw_api("unblockUser", { user_id: id })
+    end
+
+    def get_common_chats(offset_chat_id: 0, limit: 100)
+      call_raw_api("getCommonChats", { user_id: id, offset_chat_id: offset_chat_id, limit: limit })
+    end
   end
 end
