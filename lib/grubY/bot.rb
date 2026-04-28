c8ce14585fea3677c51bf4ef0abe6c3e3e609bfa
Sreyanshu <sreyanshu@mail.ru>
2026-04-28 11:43:29 +0530

    updated!

--- lib/grubY/bot.rb
+++ lib/grubY/bot.rb
@@ -1,4 +1,4 @@
-﻿require_relative "api"
+require_relative "api"
 require_relative "dispatcher"
 require_relative "middleware"
 require_relative "context"
@@ -17,8 +17,8 @@ module GrubY
       @session = session
     end
 
-    def on(event, filter=nil, &block)
-      @dispatcher.on(event, filter, &block)
+    def on(event, filter=nil, group: 0, &block)
+      @dispatcher.on(event, filter, group: group, &block)
     end
 
     def remove_handler(event, handler_id = nil, &block)
@@ -38,14 +38,53 @@ module GrubY
         ctx = Context.new(update, @api, @session)
 
         @middleware.run(ctx) do
-          if update["message"]
-            @dispatcher.trigger(:message, ctx)
-          elsif update["callback_query"]
-            @dispatcher.trigger(:callback, ctx)
-          end
+          @dispatcher.trigger(:raw_update, ctx)
+          dispatch_update_events(update, ctx)
         end
       end
+    rescue => e
+      error_ctx = Context.new({ "error" => e }, @api, @session)
+      @dispatcher.trigger(:error, error_ctx)
+      warn "[BOT] process_update error: #{e.class}: #{e.message}"
+    end
+
+    def emit(event, payload = {})
+      ctx = Context.new(payload, @api, @session)
+      @dispatcher.trigger(event, ctx)
+    end
+
+    private
+
+    def dispatch_update_events(update, ctx)
+      mapping = {
+        "message" => :message,
+        "edited_message" => :edited_message,
+        "business_message" => :business_message,
+        "edited_business_message" => :edited_business_message,
+        "deleted_messages" => :deleted_messages,
+        "deleted_business_messages" => :deleted_business_messages,
+        "message_reaction_count" => :message_reaction_count,
+        "message_reaction" => :message_reaction,
+        "business_connection" => :business_connection,
+        "story" => :story,
+        "callback_query" => :callback_query,
+        "chat_boost" => :chat_boost,
+        "chat_join_request" => :chat_join_request,
+        "chat_member_updated" => :chat_member_updated,
+        "chosen_inline_result" => :chosen_inline_result,
+        "inline_query" => :inline_query,
+        "poll" => :poll,
+        "pre_checkout_query" => :pre_checkout_query,
+        "purchased_paid_media" => :purchased_paid_media,
+        "shipping_query" => :shipping_query,
+        "user_status" => :user_status,
+        "managed_bot_updated" => :managed_bot
+      }
+
+      mapping.each do |key, event|
+        @dispatcher.trigger(event, ctx) if update.key?(key)
+      end
+      @dispatcher.trigger(:callback, ctx) if update.key?("callback_query")
     end
   end
 end
-
