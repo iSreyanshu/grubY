5173698abb69cef0c809dde1ba73ad971ad7e4f7
Sreyanshu <sreyanshu@mail.ru>
2026-04-28 11:42:50 +0530

    updated!

--- lib/grubY/bound.rb
+++ lib/grubY/bound.rb
@@ -1,11 +1,14 @@
-﻿module GrubY
+module GrubY
   module Bound
-    def self.bind(ctx)
+    module_function
+
+    def bind(ctx)
       {
-        send: ->(text){ ctx.reply(text) },
-        raw: ->(m,p){ ctx.raw(m,p) }
+        send: ->(text) { ctx.reply(text) },
+        raw: ->(method, params = {}) { ctx.raw(method, params) },
+        inline_keyboard: ->(rows) { GrubY::Keyboard.inline(rows) },
+        webapp_button: ->(text, url) { GrubY::WebApp.inline_button(text: text, url: url) }
       }
     end
   end
 end
-
